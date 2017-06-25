53fb0f988729de48fc2d59fce7f2b60ae733031a
Carlos Cordoba <carloscba1@gmail.com>
2017-06-24 23:29:42 -0300

    from .views import horario_list

--- apps/horario/urls.py
+++ apps/horario/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from views import horario_list
+from .views import horario_list
 
 urlpatterns = [
     url(r'^horario/(?P<pk>[0-9]+)$', horario_list),
